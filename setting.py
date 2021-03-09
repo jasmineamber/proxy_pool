a31b362eda05816939cd2830730f232568a4f591
jasmineamber <jasmineamber@aliyun.com>
2021-03-09 17:05:57 +0800

    new

--- setting.py
+++ setting.py
@@ -45,12 +45,12 @@ TABLE_NAME = 'use_proxy'
 
 # ###### config the proxy fetch function ######
 PROXY_FETCHER = [
-    "freeProxy01",
+    # "freeProxy01",
     "freeProxy02",
     # "freeProxy03",
     "freeProxy04",
     "freeProxy05",
-    "freeProxy06",
+    # "freeProxy06",
     "freeProxy07",
     # "freeProxy08",
     "freeProxy09",
